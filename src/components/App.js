2c135b57c9bf1b63f1faf1c6a25f7a9b4ef79f8b
ramya <mutyamramya0101@gmail.com>
2024-07-12 18:20:20 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="root">
+      <h1><p>I'm learning React. My life is getting better.</p></h1>
+    </div>
   )
 }
 
