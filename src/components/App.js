40bca2b4784a07a8b76aaf66f1c8c70f810b8230
ramya <mutyamramya0101@gmail.com>
2024-07-13 11:22:16 +0530

    k

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+// import '../styles/App.css';
 // import Comp from './Componens/comp'
 import comp from "./comp";
 
 const App = () => {
   return (
     <div id="main">
-      
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
